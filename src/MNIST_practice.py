bc8a6e72aff6d367fe8691613e083efe3314899a
WinterNH <winterNH@protonmail.com>
2023-05-05 23:02:50 -0700

    MNIST data successfully installed and tested

--- src/MNIST_practice.py
+++ src/MNIST_practice.py
@@ -1,10 +1,20 @@
 import pandas as pd
 import numpy as np
-import keras
+from tensorflow import keras
 from keras.datasets import mnist
 from matplotlib import pyplot as plt
 
+
+(train_X, train_y), (test_X, test_y) = mnist.load_data()
+print('X_train: ' + str(train_X.shape))
+print('Y_train: ' + str(train_y.shape))
+print('X_test:  '  + str(test_X.shape))
+print('Y_test:  '  + str(test_y.shape))
+
 data = pd.read_csv
 
-msg = "hello"
-print(msg)
+
+for i in range(9):  
+    plt.subplot(330 + 2 + i)
+    plt.imshow(train_X[i], cmap=plt.get_cmap('gray'))
+    plt.show()
