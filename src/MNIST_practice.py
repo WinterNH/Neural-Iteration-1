0273564ee2d326235cbc95b33f7944d9dda64df3
WinterNH <winterNH@protonmail.com>
2023-05-06 23:11:34 -0700

    reformatting

--- src/MNIST_practice.py
+++ src/MNIST_practice.py
@@ -1,5 +1,4 @@
 #%%
-
 import pandas as pd
 import numpy as np
 
@@ -18,17 +17,12 @@ from matplotlib import pyplot as plt
 from keras.datasets import mnist
 
 (train_X, train_y), (test_X, test_y) = mnist.load_data()
-print('X_train: ' + str(train_X.shape))
-print('Y_train: ' + str(train_y.shape))
-print('X_test:  '  + str(test_X.shape))
-print('Y_test:  '  + str(test_y.shape))
-
-data = pd.read_csv
+print(train_X.shape, train_y.shape)
+print(test_X.shape, test_y.shape)
 
 
-for i in range(9):  
-    plt.subplot(330 + 1 + i)
-    plt.imshow(train_X[i], cmap=plt.get_cmap('gray'))
-    plt.show()
+
+
+
 
 # %%
